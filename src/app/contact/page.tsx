09f7c2bb2f12aab5b29dd2f1475980d1574d0be7
Benjamin Tang Desktop <benjamin.code.tang@gmail.com>
2026-06-25 15:16:22 -0400

    Remove weird overflow on contact

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -23,7 +23,7 @@ const Contact = () => {
             </div>
           </div>
 
-          <div className="flex-1 flex flex-col items-start justify-start gap-24 pt-16 lg:pt-12 xl:pt-0">
+          <div className="flex-1 flex flex-col items-start justify-start gap-24 pt-16 lg:pt-12 xl:pt-0 h-full overflow-hidden">
             <div className="flex flex-col gap-10">
               <h3 className="font-victory-striker-sans lg:text-5xl text-4xl">
                 {' '}
