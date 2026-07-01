bb4276a412af60c6fb2362d267cd372fd007a8f2
Benjamin Tang Desktop <benjamin.code.tang@gmail.com>
2026-06-30 21:51:50 -0400

    Responsive main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -153,9 +153,9 @@ export default function Home() {
     <div className="bg-[#F3F9FF]">
       <TitlePage />
       <div className="bg-[#F3F9FF] px-[8%] mx-auto">
-        <div className="flex flex-col justify-center pt-24 pb-40">
-          <div className="pt-20 flex justify-between items-center">
-            <div className="flex flex-row gap-28">
+        <div className="flex flex-col justify-center pt-24 lg:pb-40 pb-28">
+          <div className="lg:pt-20 pt-16 flex flex-col gap-19 lg:gap-0 lg:flex-row justify-between items-center">
+            <div className="flex flex-row lg:gap-28 justify-between lg:justify-start w-full">
               <div className="flex flex-col lg:gap-6 gap-4">
                 <h1 className="font-victory-striker-sans lg:text-9xl text-8xl text-[#274D6F]">
                   30+
@@ -177,14 +177,14 @@ export default function Home() {
             </div>
             <div>
               <Link href="/about">
-              <div className="group text-3xl font-medium text-[#2E3F59]/90 uppercase flex flex-row items-center justify-center gap-6 hover:text-[#1C1D20] transition-colors duration-300 ease-in-out cursor-pointer">
+              <div className="group text-3xl font-medium text-[#2E3F59]/90 uppercase flex flex-row items-center justify-center gap-6 hover:text-[#1C1D20] transition-colors duration-300 ease-in-out cursor-pointer text-nowrap">
                 More About
                 <LinkArrowIcon className="w-8 fill-[#2E3F59]/90 group-hover:fill-[#1C1D20]" />
               </div>
             </Link>
             </div>
           </div>
-          <div className="font-victory-striker-sans lg:text-7xl text-5xl lg:pt-40 pt-8 flex flex-col lg:gap-6 gap-4 leading-[130%] h-full items-center lg:items-end -mx-[8%] lg:mx-auto lg:w-full">
+          <div className="font-victory-striker-sans lg:text-7xl text-5xl lg:pt-40 pt-20 flex flex-col lg:gap-6 gap-4 leading-[130%] h-full items-center lg:items-end -mx-[8%] lg:mx-auto lg:w-full">
             <h3 className="text-[#274D6F] text-center lg:text-end">
               Learn deeply,
             </h3>
